c345abf30ae57095c09b2d261d45dd278ed22331
imacks <macksme@outlook.com>
2018-12-01 19:34:04 +0800

    changed header copyright
    slightly modified language

--- src/Lizoc.JsonPlus.Tests/Source/Lizoc/JsonPlus/Tests/CoreTests.cs
+++ src/Lizoc.JsonPlus.Tests/Source/Lizoc/JsonPlus/Tests/CoreTests.cs
@@ -1,11 +1,11 @@
 ﻿// -----------------------------------------------------------------------
 // <copyright file="CoreTests.cs" repo="Json+">
 //     Copyright (C) 2018 Lizoc Inc. <http://www.lizoc.com>
-//     The source code documented in this file is subject to the MIT license.
-//     See the LICENSE file in the project root for more information.
+//     The source code in this file is subject to the MIT license.
+//     See the LICENSE file in the repository root directory for more information.
 // </copyright>
 // -----------------------------------------------------------------------
- 
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
